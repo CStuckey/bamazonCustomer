ed5314306d47d09190f780236c3d4b54cc7e264e
Chris Stuckey <chrisstuckey@Lilianas-MacBook-Pro.local>
2017-05-01 17:47:41 -0400

    Challenge 1 allows user to select product and quantity for a price

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,10 +18,8 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("This works!");
+  console.log();
   products();
-
-  connection.end();
 }); // end of connection.connect
 
 // function which displays all items from the table available for sale
@@ -29,7 +27,6 @@ var products = function() {
   // query the database for all products for sale
   connection.query("SELECT * FROM products", function(err, results) {
     if (err) throw err;
-    //console.log("Products: " + JSON.stringify(results[i].product_name));
 
     var productArray = [];
     for (var i = 0; i < results.length; i++) {
@@ -41,28 +38,50 @@ var products = function() {
         name: "choice",
         type: "input",
         message: "Which product would you like to buy. Please input the product ID.",
-        default: 0
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
       },
       {
         name: "quantity",
         type: "input",
         message: "How many units of this product would you like to purchase?",
-        default: 0
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
       }]).then(function(answer) {
       // connect with the server to locate item id based on customer choice
-      connection.query("SELECT * FROM products WHERE item_id=?", answer.choice, function(err, res){
-        if (err) throw err;
-        for ( var i = 0; i < res.length; i++) {
-          if (answer.choice === res[i].item_id) {
-          if (answer.quantity <= res[i].stock_quantity) {
-              console.log("You've purchased " + answer.quantity + " units of " + res[i].product_name);
-            } // end - if for answer.quantity
-            else {
-              console.log("There is not enough " + res[i].product_name + " availabe in stock!");
-            }
-          } // end - if for answer.choice
-        } // close for loop
-      });
+      connection.query("SELECT * FROM products WHERE item_id=?",[answer.choice], function(err, res) {
+        if (err) console.log(err);
+
+        var availableUnits;
+
+        for (var i = 0; i < res.length; i++) {
+          var chosenPrice = res[i].price * answer.quantity;
+          console.log("Total price $" + chosenPrice);
+
+          availableUnits = res[i].stock_quantity - parseInt(answer.quantity);
+          console.log(availableUnits + " units are remaining and available.");
+          console.log("_____________________________________________");
+        } // end of for loop
+
+        // determine if quantity is available and deliver price
+        connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [availableUnits, parseInt(answer.choice)], function(error, result) {
+          if (error) throw error;
+
+          products();
+        }) // end function from 2nd connect query
+
+      }); // item_id connection.query
+
+          console.log("Item Id " + answer.choice + " Quantity " + answer.quantity);
+          // console.log(answer.quantity * res[i].price);
 
     }) // end of answer function
   }); // end function err, results
